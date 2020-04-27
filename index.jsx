4d57c8fcbb4c9833d69c8e586ace7b8c537f853d
cameronfr <catechboy@hotmail.com>
2020-04-27 20:55:38 +0900

    switch to typedarrays

--- index.jsx
+++ index.jsx
@@ -2,7 +2,88 @@ var React = require("react")
 var ReactDOM = require("react-dom")
 import Regl from "regl"
 import mat4 from "gl-mat4"
+import ndarray from "ndarray"
+import ndops from "ndarray-ops"
 
+var ndSlice = (ndarr, from, to) => {
+  var lengths = to.map((t, idx) => from[idx] - t)
+  return ndarr.lo(...from).hi(...lengths)
+}
+
+function LinesState (regl) {
+  this.regl = regl
+  var arrayLength = 1000 // length in number of points
+  var points = ndarray(new Uint16Array(arrayLength*2), [arrayLength, 2])
+  var widths = ndarray(new Uint16Array(arrayLength), [arrayLength])
+  var normals = ndarray(new Int16Array(arrayLength*2), [arrayLength, 2])
+  var lineIds = ndarray(new Uint16Array(arrayLength))
+  var currentLineId = 0 // blank line
+  var currentIdx = 0
+
+  this.startNewLine = () => {
+    currentLineId = 1
+    currentIdx = 0
+  }
+  this.addPoint = ([x, y, width]) => {
+    var [lastX, lastY] = [points.get(...[currentIdx-1], 0), points.get(...[currentIdx-1], 1)]
+    var normal = [-(y - lastY), x - lastX]
+    normals.set(...[currentIdx, 0], normal[0])
+    normals.set(...[currentIdx, 1], normal[1])
+    points.set(...[currentIdx, 0], x)
+    points.set(...[currentIdx, 1], y)
+    widths.set(...[currentIdx], width)
+    lineIds.set(...[currentIdx], currentLineId)
+    currentIdx += 1
+  }
+
+  var buffers = {}
+  var bufferSet1 = {"point": points, "width": widths, "normal": normals, "lineId": lineIds}
+  var bufferSet2 = {"normalMultiplier": null}
+  Object.keys({...bufferSet1, ...bufferSet2}).forEach(name => {
+    buffers[name] = this.regl.buffer()
+  })
+
+  var normalDirections = ndarray(new Int16Array(arrayLength * 2))
+  ndops.assigns(ndSlice(normalDirections, [0], [arrayLength]), 1)
+  ndops.assigns(ndSlice(normalDirections, [arrayLength], [arrayLength*2]), -1)
+  buffers["normalMultiplier"]({
+    data: Array.from(normalDirections.data), type: "int8"
+  })
+
+  this.elements = () => {
+    var numPoints = currentIdx
+    var elements = []
+    if (numPoints < 2) {return ({elements})}
+    var elements = new Uint16Array((numPoints - 1) * 2 * 3)
+    for (var idx =0; idx<numPoints-1; idx++) {
+      var tri1 = [idx, idx+arrayLength + 1, idx+1]
+      elements.set(tri1, 6*idx)
+      var tri2 = [idx+arrayLength, idx+arrayLength+1, idx]
+      elements.set(tri2, 6*idx + 3)
+    }
+    return {elements}
+  }
+
+  this.attributes = () => {
+    // updates the buffers
+    Object.keys(bufferSet1).forEach(name => {
+      //TODO: update buffers instead of creating new
+      var typedArray = bufferSet1[name].data
+      // need to double points for this line method. matches type of typedarray
+      var doubledTypedArray = new typedArray.constructor(typedArray.length * 2)
+      doubledTypedArray.set(typedArray, 0)
+      doubledTypedArray.set(typedArray, typedArray.length)
+      if (name == "normal") {
+      }
+      buffers[name]({
+        data: doubledTypedArray
+      })
+    })
+    return buffers
+  }
+
+
+}
 
 var App = props => {
   var containerRef = React.useRef()
@@ -37,10 +118,14 @@ var App = props => {
     addEventListener(window, "resize", resizeCanvas)
 
     // pencil & touch & click fallback
+    var linesState = new LinesState(regl)
     var pencilOnPaper = false
     var currentLine = []
     var handleTouch = (type, e) => {
-      if (type == "start") {pencilOnPaper = true}
+      if (type == "start") {
+        pencilOnPaper = true
+        linesState.startNewLine()
+      }
       if (type == "end") {pencilOnPaper = false}
 
       e.preventDefault()
@@ -54,13 +139,11 @@ var App = props => {
       var [x, y] = screenSpaceToCanvasSpace(e)
 
       if (pencilOnPaper) {
-        const minDist = 1
-        var lastPoint = currentLine[currentLine.length - 1] || [-minDist, -minDist]
-        if (Math.abs(lastPoint[0] - x) > minDist || Math.abs(lastPoint[1]-y) > minDist) {
-          currentLine.push([x, y])
-        }
-      } else {
-        currentLine.length = 0 // clear & preserve ref
+        // const minDist = 1
+        // var lastPoint = currentLine[currentLine.length - 1] || [-minDist, -minDist]
+        // if (Math.abs(lastPoint[0] - x) > minDist || Math.abs(lastPoint[1]-y) > minDist) {
+          linesState.addPoint([x, y, 3])
+        // }
       }
     }
     ;["touchstart", "mousedown"].forEach(name => addEventListener(canvas, name, e => handleTouch("start", e)))
@@ -75,22 +158,25 @@ var App = props => {
     mat4.translate(matrix, matrix, [-0.5, -0.5, 0])
     mat4.scale(matrix, matrix, [1/canvasRect.width, 1/canvasRect.height, 1])
     var canvasSpaceToGlSpace = matrix
-    console.log(canvasSpaceToGlSpace)
 
-    const drawTriangle = regl({
+    var attributeFunctions = {}
+    Object.keys(linesState.attributes()).forEach(name => {
+      attributeFunctions[name] = (context, props) => props[name]
+    })
+
+    const drawLines = regl({
       vert: `
         precision mediump float;
-        attribute vec2 position;
-        attribute vec2 positionNext;
+        attribute vec2 point;
+        attribute vec2 normal;
+        attribute float width;
         attribute float normalMultiplier;
         uniform mat4 projection;
         void main() {
-          vec2 segmentDelta = positionNext - position;
-          vec2 normal = normalize(vec2(-segmentDelta.y, segmentDelta.x));
           // vec2 normal = vec2(0.7, 0.7);
-          const float width = 10.0;
+          vec2 normedNormal = normalize(normal);
 
-          vec2 pointPosition = width * normal * normalMultiplier + position;
+          vec2 pointPosition = width * normedNormal * normalMultiplier + point;
 
           gl_Position = projection * vec4(pointPosition, 0, 1);
         }`,
@@ -102,36 +188,6 @@ var App = props => {
           gl_FragColor = color;
         }`,
 
-      attributes: {
-        position: () => {
-          var buffer = regl.buffer({
-            data: currentLine.concat(currentLine)
-          })
-          return buffer
-        },
-        positionNext: () => {
-          var points = currentLine.concat(currentLine)
-          if (points.length > 1) {
-            points.push(points[points.length-2])
-            points = points.slice(1, points.length)
-          }
-          var buffer = regl.buffer({
-            data: points
-          })
-          return buffer
-        },
-        normalMultiplier: () => {
-          var lefts = currentLine.map(x => -1)
-          lefts[lefts.length - 1] = 1 // because of positionNext, last pt will have linedir flipped
-          var rights = currentLine.map(x => 1)
-          rights[rights.length - 1] = -1
-          var buffer = regl.buffer({
-            data: lefts.concat(rights)
-          })
-          return buffer
-        }
-      },
-
       uniforms: {
         projection: canvasSpaceToGlSpace,
         color: () => ([
@@ -142,21 +198,13 @@ var App = props => {
         ])
       },
 
-      elements: () => {
-        var elements = []
-        for (var idx =0; idx<currentLine.length-1; idx++) {
-          var tri1 = [idx, idx+currentLine.length + 1, idx+1]
-          var tri2 = [idx+currentLine.length, idx+currentLine.length+1, idx]
-          elements.push(tri1, tri2)
-        }
-        // console.log(elements)
-        return elements
+      attributes: {
+        ...attributeFunctions
       },
 
+      elements: (context, props) => props.elements,
+
       primitive: "triangles",
-      // primitive: "line strip",
-      // lineWidth: 1,
-      // count: () => (currentLine.length-1)*6,
     })
     cleanupFunctions.push(regl.destroy)
 
@@ -167,9 +215,11 @@ var App = props => {
         color: [0, 0, 0, 0],
         depth: 1
       })
-      // draw a triangle using the command defined above
-      drawTriangle({
-      })
+      var props = {
+        ...linesState.attributes(),
+        ...linesState.elements(),
+      }
+      drawLines(props)
       animationFrameRequestId = window.requestAnimationFrame(tick)
     }
     var animationFrameRequestId = window.requestAnimationFrame(tick)
